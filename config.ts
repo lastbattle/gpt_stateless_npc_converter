6e9d79ecee5a2d52dcdbfb517c8b471e2f4edc3a
lastbattle <4586194+lastbattle@users.noreply.github.com>
2023-03-09 13:25:09 +0800

    cleanupp unused packages

---
+++ config.ts
@@ -0,0 +1,25 @@
+
+export class Config {
+
+    // only `gpt-3.5-turbo` and `gpt-3.5-turbo-0301` 
+    public static readonly OPENAI_CHATGPT_MODEL: string = "gpt-3.5-turbo";
+     // Controls randomness: Lowering results in less random completions. As the temperature approaches zero, the model will become deterministic and repetitive.
+     public static readonly GPT3_5_CONFIG_TEMPERATURE: number = 0; 
+     // The maximum number of tokens to generate. By default, this is 150.
+     public static readonly GPT3_5_CONFIG_MAXTOKENS: number = 1000;
+     // An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+     public static readonly GPT3_5_CONFIG_TOPP: number = 1;
+     // How much to penalize new tokens based on their existing frequency in the text so far. (Higher means the model will avoid repeating the same line over and over).
+     public static readonly GPT3_5_CONFIG_FREQUENCYPENALTY: number = 0;
+     // How much to penalize new tokens based on whether they appear in the text so far. (Higher means the model will avoid repeating the same line over and over).
+     public static readonly GPT3_5_CONFIG_PRESENCEPENALTY: number = 0;
+
+
+    // https://platform.openai.com/account/api-keys
+    public static readonly OPENAI_API_KEY: string = '<replace with your own api key>';
+
+    public static readonly INPUT_FOLDER_PROMPT: string = "prompts";
+    public static readonly INPUT_FOLDER: string = "_input";
+    public static readonly OUTPUT_FOLDER: string = "_output";
+
+}
--- script.ts
+++ script.ts
@@ -1,4 +1,4 @@
-import { Constants } from "./constants";
+import { Config } from "./config";
 
 const { Configuration, OpenAIApi } = require("openai");
 
@@ -7,7 +7,7 @@ const path = require('path');
 
 // get your API key here https://platform.openai.com/account/api-keys
 const configuration = new Configuration({
-  apiKey: Constants.OPENAI_API_KEY,
+  apiKey: Config.OPENAI_API_KEY,
 });
 
 // main
@@ -27,13 +27,13 @@ const main = async () => {
   const openai = new OpenAIApi(configuration);
 
   // read a single file from folder
-  const prompt_system_1: string = readFileSync(path.join(__dirname, 'prompts/1_system.txt'), 'utf8');
-  const prompt_system_2: string = readFileSync(path.join(__dirname, 'prompts/2_system.txt'), 'utf8');
-  const prompt_user_3: string = readFileSync(path.join(__dirname, 'prompts/3_user.txt'), 'utf8');
-  const prompt_unconverted_stateless_npc_example: string = readFileSync(path.join(__dirname, 'prompts/9110107.js'), 'utf8');
-  const prompt_converted_stateless_npc_example: string = readFileSync(path.join(__dirname, 'prompts/9110107_converted_stateless.js'), 'utf8');
-  const prompt_assistant_4: string = readFileSync(path.join(__dirname, 'prompts/4_assistant.txt'), 'utf8');
-  const prompt_user_5: string = readFileSync(path.join(__dirname, 'prompts/5_user.txt'), 'utf8');
+  const prompt_system_1: string = readFileSync(path.join(__dirname, Config.INPUT_FOLDER_PROMPT, '1_system.txt'), 'utf8');
+  const prompt_system_2: string = readFileSync(path.join(__dirname, Config.INPUT_FOLDER_PROMPT, '2_system.txt'), 'utf8');
+  const prompt_user_3: string = readFileSync(path.join(__dirname, Config.INPUT_FOLDER_PROMPT, '3_user.txt'), 'utf8');
+  const prompt_unconverted_stateless_npc_example: string = readFileSync(path.join(__dirname, Config.INPUT_FOLDER_PROMPT, '9110107.js'), 'utf8');
+  const prompt_converted_stateless_npc_example: string = readFileSync(path.join(__dirname, Config.INPUT_FOLDER_PROMPT, '9110107_converted_stateless.js'), 'utf8');
+  const prompt_assistant_4: string = readFileSync(path.join(__dirname, Config.INPUT_FOLDER_PROMPT, '4_assistant.txt'), 'utf8');
+  const prompt_user_5: string = readFileSync(path.join(__dirname, Config.INPUT_FOLDER_PROMPT, '5_user.txt'), 'utf8');
   
   /*
   * 1. [System] Prompts GPT what persona it needs to be, or to act as. 
@@ -44,7 +44,7 @@ const main = async () => {
   */
 
   // read files from npc folder 
-  const dir = path.join(__dirname, '_input/npc/');
+  const dir = path.join(__dirname, Config.INPUT_FOLDER, 'npc/');
   const files = readdirSync(dir);
   // end
   //console.log(files);
@@ -54,7 +54,7 @@ const main = async () => {
     if (npc_file_name.endsWith('.js')) {
       console.log(npc_file_name);
 
-      const script: string = readFileSync(path.join(__dirname, '_input/npc/' + npc_file_name), 'utf8');
+      const script: string = readFileSync(path.join(__dirname, Config.INPUT_FOLDER, 'npc/' + npc_file_name), 'utf8');
 
       // https://javascript.plainenglish.io/getting-started-with-openai-api-in-javascript-d1bc365069f0
       // https://platform.openai.com/docs/libraries/node-js-library
@@ -82,12 +82,12 @@ const main = async () => {
               "content": prompt_user_5.replace('{ENTER_SCRIPT_TO_CONVERT_HERE}', script)
             },
           ],
-          "temperature": 0, // Controls randomness: Lowering results in less random completions. As the temperature approaches zero, the model will become deterministic and repetitive.
-          "max_tokens": 1000, // The maximum number of tokens to generate. By default, this is 150.
-          "top_p": 1, // An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
-          "frequency_penalty": 0, // How much to penalize new tokens based on their existing frequency in the text so far. (Higher means the model will avoid repeating the same line over and over).
-          "presence_penalty": 0, // How much to penalize new tokens based on whether they appear in the text so far. (Higher means the model will avoid repeating the same line over and over).
-          "model": Constants.OPENAI_CHATGPT_MODEL, // The model to use. One of ada, babbage, curie, davinci, or content-filter-alpha-c4.
+          "temperature": Config.GPT3_5_CONFIG_TEMPERATURE, // Controls randomness: Lowering results in less random completions. As the temperature approaches zero, the model will become deterministic and repetitive.
+          "max_tokens": Config.GPT3_5_CONFIG_MAXTOKENS, // The maximum number of tokens to generate. By default, this is 150.
+          "top_p": Config.GPT3_5_CONFIG_TOPP, // An alternative to sampling with temperature, called nucleus sampling, where the model considers the results of the tokens with top_p probability mass. So 0.1 means only the tokens comprising the top 10% probability mass are considered.
+          "frequency_penalty": Config.GPT3_5_CONFIG_FREQUENCYPENALTY, // How much to penalize new tokens based on their existing frequency in the text so far. (Higher means the model will avoid repeating the same line over and over).
+          "presence_penalty": Config.GPT3_5_CONFIG_PRESENCEPENALTY, // How much to penalize new tokens based on whether they appear in the text so far. (Higher means the model will avoid repeating the same line over and over).
+          "model": Config.OPENAI_CHATGPT_MODEL, // The model to use. One of ada, babbage, curie, davinci, or content-filter-alpha-c4.
           "stream": false // Whether to stream back partial progress or not. If set to false, the API will only return a response when the request is complete.
         });
 
@@ -113,15 +113,15 @@ const main = async () => {
         const output_gpt_script = response.data.choices[0].message.content;
         console.log(output_gpt_script);
         // create a file and write its contents
-        if (!existsSync(path.join(__dirname, '_output/npc'))) {
-          mkdirSync(path.join(__dirname, '_output/npc'));
+        if (!existsSync(path.join(__dirname, Config.OUTPUT_FOLDER, 'npc'))) {
+          mkdirSync(path.join(__dirname, Config.OUTPUT_FOLDER, 'npc'));
         }
         // write output_gpt_script to file
 
 
 
         // write the output scripts 
-        writeFileSync(path.join(__dirname, '_output/npc/' + npc_file_name), output_gpt_script);
+        writeFileSync(path.join(__dirname, Config.OUTPUT_FOLDER, 'npc/' + npc_file_name), output_gpt_script);
       } catch (e) {
         console.log(e.toString());
       }
