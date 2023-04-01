e0b8175f5681ccf7a9c9ebd9c036b52c9dbc9e95
lastbattle <4586194+lastbattle@users.noreply.github.com>
2023-04-01 20:21:41 +0800

    better & smaller prompts

--- prompts/9110107_converted_stateless.js
+++ prompts/9110107_converted_stateless.js
@@ -3,12 +3,12 @@ function start() {
     
     if (cm.getMapId() == 800040000) {
         if (cm.askYesNo("Oh what? What is it? Do you want to go visit Mushroom Shrine?") == 1) {
-            cm.say("Okay, I got it! Just let us do the work, and you'll get there in the blink of an eye! Oh, and this won't cost you any money. Today's a good day for me, so I'll just let you get on it for free! Now, doesn't that make you feel good or what? Anyway, off we go!");
+            cm.say("Okay, I got it! Just let us do the work, and you'll get there in the blink of an eye! Oh, and this won't cost you any money.");
             cm.warp(800000000);
         }
     } else {
         if (cm.askYesNo("Oh what? What is it? Do you want to go visit Ninja Castle") == 1) {
-            cm.say("Okay, I got it! Just let us do the work, and you'll get there in the blink of an eye! Oh, and this won't cost you any money. Today's a good day for me, so I'll just let you get on it for free! Now, doesn't that make you feel good or what? Anyway, off we go!");
+            cm.say("Okay, I got it! Just let us do the work, and you'll get there in the blink of an eye! Oh, and this won't cost you any money.");
             cm.warp(800040000);
         }
     }
